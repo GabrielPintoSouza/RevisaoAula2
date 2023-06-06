93832b2d95bfc13a9f836b427de77609a2f7e29b
GabrielPS <gabriel.pinto@cefet-rj.br>
2023-06-06 14:31:27 -0300

    leitura da operacao

--- CalculadoraBranchs/main.c
+++ CalculadoraBranchs/main.c
@@ -3,13 +3,17 @@
 
 int main()
 {
+    char operador;
     int num1, num2;
     printf("Programa: Calculadora:\n");
+    printf("Digite a operacao que deseja realizar: +(soma) -(subtracao) *(multiplicacao) /(divisao)\n")
+    scanf("%c", &operador);
     printf("Digite o primeiro numero:\n");
     scanf("%d", &num1);
     printf("Digite o segundo numero:\n");
     scanf("%d", &num2);
 
+    printf("A operacao escolhida foi: %c", operador);
     printf("Numero um selecionado:%d  Numero dois selecionado:%d\n", num1, num2);
     return 0;
 }
