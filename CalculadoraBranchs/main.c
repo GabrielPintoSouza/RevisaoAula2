da531b2e2d524f8e3fcb96c02928f3f8e993ee38
GabrielPS <gabriel.pinto@cefet-rj.br>
2023-06-06 14:46:57 -0300

    Retorno da resposta implementada

--- CalculadoraBranchs/main.c
+++ CalculadoraBranchs/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+double Calcula(int num1, int num2, char operador){
+    switch(operador){
+        case '+': return num1 + num2;
+        case '-': return num1 - num2;
+        case '*': return num1 * num2;
+        case '/': return num1 / num2;
+        default: return 0;
+    }
+}
 int main()
 {
     char operador;
@@ -13,7 +21,8 @@ int main()
     printf("Digite o segundo numero:\n");
     scanf("%d", &num2);
 
-    printf("A operacao escolhida foi: %c", operador);
+    printf("A operacao escolhida foi: %c ", operador);
     printf("Numero um selecionado:%d  Numero dois selecionado:%d\n", num1, num2);
+    printf("O resultado da operacao foi: %f", Calcula(num1, num2, operador));
     return 0;
 }
